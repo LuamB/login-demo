daceb2336279103c1b1d5371b79ec23b1084c11e
Luam Belay <luam.belay@gmail.com>
2024-02-12 19:38:53 +0100

    GET /

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import path, { dirname } from "path";
 
-// Reads the PORT value from the environment variable `PORT`.
+// Reads PORT value from environment variable `PORT`.
 // If not found, uses the default value of 3000.
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// Define 'public' dir for static files (including HTML).
+// Allows readable access from the browser.
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+app.use(express.static(path.join(__dirname, "public")));
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
