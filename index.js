a3d0f8ab77698dcef22dfe80a61170408871c71d
Luam Belay <luam.belay@gmail.com>
2024-02-12 21:05:57 +0100

    using middleware to get styling

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import express from "express";
+import { URL } from "url";
 import path, { dirname } from "path";
+import express, { response } from "express";
 
-// Reads PORT value from environment variable `PORT`.
-// If not found, uses the default value of 3000.
+// Initialize server & listen
+// Reads PORT value from environment variable `PORT`, default value is 3000.
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.listen(PORT, () => console.log(`Server listening on post ${PORT}`));
 
-// Define 'public' dir for static files (including HTML).
-// Allows readable access from the browser.
+// 1. GET / using express.static() middleware,
+// serving static files (like HTML, CSS, or JS) from specified dir (here, "public")
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 app.use(express.static(path.join(__dirname, "public")));
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
-});
+// 1. GET / with explicit route handler
+// app.get("/", (req, res) => {
+//   console.log("serving index.html...");
+//   res.sendFile(path.join(__dirname, "public", "/index.html"));
+// });
